f48c7fdeee180814744b9ed452cce7040089bb6c
Peter Ajala <ajalapeter1@gmail.com>
2024-05-03 05:26:30 -0700

    worked on conditional statement

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function ageCalculator() {
     var username = uName.value
     var yob = yearOfBirth.value;
     var age = 2024 - yob;
-    var ageStatment = username + " your age is: " + age;
-    alert(ageStatment)
+
+    if (age >= 18){
+        alert("You are an Adult")
+    } else if (age > 13 && age < 18){
+        alert("You are a Teenager")
+    } else if (age < 13 && age > 0 ){
+        alert("You are a Kid")
+    } else{
+        alert ("You category is not found")
+    }
+    // var ageStatment = username + " your age is: " + age;
+    // alert(ageStatment)
 }
